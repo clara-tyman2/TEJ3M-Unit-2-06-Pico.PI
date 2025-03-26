a7ee0ed7d771dd428a6cfb9092adea2a8a39ff72
clara-tyman2 <clara.tyman@stu.ocsb.ca>
2025-03-26 10:00:44 -0400

    stupid is stillnot working

--- code.py
+++ code.py
@@ -1,3 +1,37 @@
-# Created by: Clara T
-# Created on: Mar 2025
-# This program uses sonar
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(trigPin, GPIO.OUT)
+GPIO.setup(echoPin, GPIO.IN)
+led = digitalio.DigitalInOut(board.GP5)
+
+
+try:
+    while True:
+        GPIO.output(trigPin, GPIO.LOW)
+        time.sleep(0.5)
+        GPIO.output(trigPin, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(trigPin, GPIO.LOW)
+
+        start_time = time.time()
+        while GPIO.input(echoPin) == 0:
+            start_time = time.time()
+
+        while GPIO.input(echoPin) == 1:
+            stop_time = time.time()
+
+        duration = stop_time - start_time
+        distance = (duration * 34300) / 2
+
+        if distance < 10:
+            GPIO.output(led, GPIO.HIGH)
+        else:
+            GPIO.output(led, GPIO.LOW)
+
+        print(distance, "cm")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
